dc58176da171ee902ecb5adb05f57e62262c4a29
Glitch (eminent-fact) <none>
2018-10-21 14:26:11 +0000

    🎎👴 Checkpoint ./myApp.js:1715671/156 ./server.js:1715671/244

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
 
 
 var cors = require('cors');
@@ -15,11 +14,13 @@ var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
 // mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGO_URI);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+var bodyParser = require('body-parser');
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -29,6 +30,11 @@ app.get('/', function(req, res){
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+app.post("/api/shorturl/new", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
