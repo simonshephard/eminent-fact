595ea3e7e0791d41393039b5e7336942a21c696f
Glitch (eminent-fact) <none>
2018-10-21 13:53:12 +0000

    ☄️💃 Checkpoint ./myApp.js:1715671/869

--- myApp.js
+++ myApp.js
@@ -1,11 +1,32 @@
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI);
 
+
+// set up scheme for shortUrl
 var Schema = mongoose.Schema;
 var shortUrlSchema = new Schema({
   longUrl: {type: String,
          required: true},
-  shortongUrl: {type: String,
+  shortUrl: {type: String,
          required: true}
 });
-var Person = mongoose.model('Person', personSchema);
+var ShortUrl = mongoose.model('ShortUrl', shortUrlSchema);
+
+
+
+var createAndSaveShortUrl;
+
+
+var findUrlById = function(UrlId, done) {
+  
+  ShortUrl.findById(UrlId, function(err, data) {
+    if (err) return done(err);
+    return done(null, data);
+  });
+  
+};
+
+
+exports.ShortUrlModel = ShortUrl;
+exports.createAndSaveShortUrl = createAndSaveShortUrl;
+exports.findUrlById = findUrlById;
